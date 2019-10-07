ceca0a4596955c520e400ddef26251cc28a38774
Claudio Givisiez <claudio.givisiez@amedigital.com>
2019-10-07 16:08:13 -0300

    Changing log

--- calc/math.go
+++ calc/math.go
@@ -7,7 +7,7 @@ func Add(numbers ...int) (error, int) {
 	sum := 0
 
 	if len(numbers) < 2 {
-		return errors.New("provide more than two numbers"), sum
+		return errors.New("Provide more than two numbers"), sum
 	} else {
 		for _, num := range numbers {
 			sum = sum + num
